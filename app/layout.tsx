c8a193748c643ec892a5ec0bd78d77e2957a5978
James Kekeli <jameskekeli12@gmail.com>
2026-04-09 13:40:25 +0000

    package:useSWR for API methods

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
-
+import { Geist, Geist_Mono,Inter } from "next/font/google"
+//@ts-ignore
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+//const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
-const fontMono = Geist_Mono({
+const geistMono = Geist_Mono({
   subsets: ["latin"],
-  variable: "--font-mono",
+  variable: "--font-geist-mono",
+})
+
+const geistSans = Geist({
+  variable:"--font-geist-sans",
+  subsets:["latin"],
 })
 
 export default function RootLayout({
@@ -20,7 +25,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", geistMono.variable, geistSans.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
