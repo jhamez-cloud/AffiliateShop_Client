1d9609c4479a7240d73f9259e27b7c7344556ac1
James Kekeli <jameskekeli12@gmail.com>
2026-04-19 22:47:36 +0000

    fix: post method fix and post payload typing

--- lib/api.ts
+++ lib/api.ts
@@ -1,15 +1,46 @@
+import { CreateUserPayload } from "@/types/payload"
+import { getAuth } from "firebase/auth"
+
+const Django_Url = process.env.NEXT_PUBLIC_DJANGO_URL
+
 //Create user trigger function
-export const createuser = async (endpoint:string,{arg}:any) => {
+export const createuser = async (endpoint:string,{arg}:{arg:CreateUserPayload}) => {
 
-    const Django_Url = process.env.NEXT_PUBLIC_DJANGO_URL
-
-    const res = await fetch(`${Django_Url}/api/v1/${endpoint}`,{
+    const res = await fetch(`${Django_Url}/api/v1/${endpoint}/`,{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         credentials:"include",
         body:JSON.stringify(arg)
     })
 
-    if(!res.ok) throw new Error("Failed To Create User Account")
+    if (!res.ok) {
+      const errorData = await res.json()
+      console.error("Backend error:", errorData)
+      throw new Error(errorData.detail || "Failed To Create User Account")
+    }
+
     return res.json()
+}
+
+export const fetchNotifications = async (endpoint: string,token:string) => {
+  const auth = getAuth()
+  const user = auth.currentUser
+
+  if (!user) {
+    throw new Error("Firebase user not ready yet")
+  }
+  console.log("TOKEN:", token)
+
+  const res = await fetch(`${Django_Url}/api/v1/${endpoint}/`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  if (!res.ok) throw new Error("Failed to fetch")
+  
+  const notificationData = await res.json()
+
+  console.log(notificationData)
+  return notificationData
 }
---
+++ types/payload.ts
@@ -0,0 +1,6 @@
+export type CreateUserPayload = {
+  firebase_uid: string
+  email: string
+  fullname?: string
+  phone?: string
+}
